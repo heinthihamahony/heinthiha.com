69bfc56fef86294188531fa076f68d54bd26f7b1
Hein Thiha <heinthiha@Heins-MacBook-Air.local>
2024-05-18 23:14:11 +0700

    heinthiha.com

--- app/component/ui/md-nav.tsx
+++ app/component/ui/md-nav.tsx
@@ -9,7 +9,7 @@ export function MdSmNavbar() {
           <div className=" flex items-center gap-3">
             <img
               className="rounded-full w-14 border-4 border-[#62626b]"
-              src="https://scontent.fbkk22-7.fna.fbcdn.net/v/t39.30808-1/438205394_1168651504317448_1342600203547611987_n.jpg?stp=dst-jpg_p148x148&_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeE8ydvZhtTHAiO3EQ1kezWiTH91wYvcvytMf3XBi9y_K-dprcgeMxyqgplh2MKrb9TBsb9ok3nvpZu2tbu6GPNN&_nc_ohc=j8e3zFkC0JMQ7kNvgHVlABR&_nc_ht=scontent.fbkk22-7.fna&oh=00_AYC2507Qyk1qUUtExPsYJffDF5Z5IbkhjfSNGaI-yfQw9Q&oe=6645456B"
+              src="https://avatars.githubusercontent.com/u/145444401?v=4"
               alt=""
             />
 
